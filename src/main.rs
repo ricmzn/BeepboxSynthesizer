1f452b1836084ab3e81bd9947e1d67f5fe4ff108
Ricardo Maes <ricmzn@gmail.com>
2024-05-30 23:50:47 +0100

    Fix Windows build

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use std::env;
-use std::os::unix::process::CommandExt;
-use std::process::{Command, exit};
+#[cfg(unix)]
+fn exec_godot() {
+    use std::env;
+    use std::os::unix::process::CommandExt;
+    use std::process::{exit, Command};
 
-fn main() {
     match env::var("GODOT_BIN") {
         Ok(godot_bin) => {
             let error = Command::new(&godot_bin).args(["--path", "."]).exec();
@@ -13,4 +14,13 @@ fn main() {
             exit(1);
         }
     }
+}
+
+#[cfg(not(unix))]
+fn exec_godot() {
+    println!("This binary is only used to launch the Godot editor for debugging, using exec(), but unfortunately, this function is not supported in your platform.");
+}
+
+fn main() {
+    exec_godot()
 }
